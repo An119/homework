36ae81d8b0ecc5337f14f2f40de03275896da5fb
Anna <118921357+An119@users.noreply.github.com>
2026-04-05 13:22:36 +0500

    Add files via upload

---
+++ JS5/main.js
@@ -0,0 +1,78 @@
+// Задача 1.
+// Создайте объект person с несколькими свойствами, содержащими информацию о вас. Затем выведите значения этих свойств в консоль.
+
+// ----------------РЕШЕНИЕ----------------
+const person = {
+  name: "Anna",
+  birthYear: 2003,
+  age: 22,
+  city: "Kamensk-Uralsky",
+};
+
+console.log(`Hello, my name is ${person.name}, i'm ${person.age} years old`);
+// ---------------------------------------
+
+// Задача 2.
+// Создайте функцию isEmpty, которая проверяет является ли переданный объект пустым. Если объект пуст - верните true, в противном случае false.
+
+// ----------------РЕШЕНИЕ----------------
+const object = {}
+
+function isEmpty(object) {
+  for (let key in object) {
+    return false; //объект не пуст
+  }
+  return true; //объект пуст
+}
+
+console.log(isEmpty(object));
+// ---------------------------------------
+
+// Задача 3.
+// Создайте объект task с несколькими свойствами: title, description, isCompleted.
+// Напишите функцию cloneAndModify(object, modifications), которая с помощью оператора spread создает копию объекта и применяет изменения из объекта modifications.
+// Затем с помощью цикла for in выведите все свойства полученного объекта.
+
+// ----------------РЕШЕНИЕ----------------
+const task = {
+  title: "Изучить объекты",
+  description: "Научиться работать с объектами",
+  isCompleted: false
+};
+
+function cloneAndModify(object, modifications) {
+  return { ...object, ...modifications };
+}
+
+const task2 = cloneAndModify(task);
+
+for (const key in task2) {
+  console.log(task2[key]);
+}
+// ---------------------------------------
+
+// Задача 4.
+// Создайте функцию callAllMethods, которая принимает объект и вызывает все его методы.
+
+// Пример использования:
+const myObject = {
+    method1() {
+        console.log('Метод 1 вызван');
+    },
+    method2() {
+        console.log('Метод 2 вызван');
+    },
+    property: 'Это не метод'
+};
+
+// ----------------РЕШЕНИЕ----------------
+function callAllMethods(myObject) {
+  for (let key in myObject) {
+    if (typeof myObject[key] === 'function') {  // проверка чтобы значение было свойством функции
+      myObject[key]();
+    }
+  }
+}
+callAllMethods(myObject);
+// ---------------------------------------
+
